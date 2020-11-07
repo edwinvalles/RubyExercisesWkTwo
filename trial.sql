9482ac0807ec58d5e0cf2e5a226726f2045ce549
edwinvalles <edwinvalles@gmail.com>
2020-11-07 08:51:43 +0800

    database exercise completion

--- trial.sql
+++ trial.sql
@@ -1,4 +1,8 @@
+-- create new table
+
 CREATE TABLE students (id integer, first_name varchar, middle_name varchar, last_name varchar, age integer, location_name varchar);
+
+-- insert data 
 
 INSERT INTO students (id integer, first_name varchar, middle_name varchar, last_name varchar, age integer, location_name varchar)
 VALUES (1, "Juan", "Blank", "Cruz", 18, "Manila");
@@ -18,9 +22,13 @@ VALUES (5, "Pau", "Blank", "Riosa", 22, "Marikina");
 INSERT INTO students (id integer, first_name varchar, middle_name varchar, last_name varchar, age integer, location_name varchar)
 VALUES (6, "Piolo", "Blank", "Pascual", 25, "Manila");
 
+-- update data 
+
 UPDATE students
 SET first_name: "Ana", middle_name: "Cui", last_name: "Cajocson", age: 25, location_name: "Bulacan"
 WHERE id=1;
+
+-- delete data 
 
 DELETE FROM students
 WHERE id=6;
